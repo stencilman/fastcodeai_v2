10dc2170dcec643d9213f6af44d6653e347deca2
abdulfastcode <abdul@fastcode.ai>
2024-05-15 02:20:30 +0530

    added links to latest blog

--- src/app/blogs/sections/LatestBlogs.jsx
+++ src/app/blogs/sections/LatestBlogs.jsx
@@ -1,4 +1,4 @@
-
+import Link from "next/link";
 import React from "react";
 
 const LatestBlogs = () => {
@@ -11,146 +11,161 @@ const LatestBlogs = () => {
       </div>
 
       <div className="grid grid-cols-3 grid-rows-3 gap-7 w-full">
-        <div className="col-span-2">
-          <div className="w-[833px] h-[327px] bg-no-repeat bg-blog1 relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                The Dilemma: To ML or Not to ML?
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        <Link href="/blogs/why-ai">
+          <div className="col-span-2">
+            <div className="w-[833px] h-[327px] bg-no-repeat bg-blog1 relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  The Dilemma: To ML or Not to ML?
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-start-3 ">
-          <div className="bg-[#104E62] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                Oil & Gas EPC - Digitizing P&ID
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+
+        <Link href="/blogs/why-ai">
+          <div className="col-start-3 ">
+            <div className="bg-[#104E62] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  Oil & Gas EPC - Digitizing P&ID
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row-start-2">
-          <div className="w-[402px] h-[328px] bg-no-repeat bg-blog2 relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                How to deal with Unknown Classes during classification?
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+        <Link href="/blogs/why-ai">
+          <div className="row-start-2">
+            <div className="w-[402px] h-[328px] bg-no-repeat bg-blog2 relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  How to deal with Unknown Classes during classification?
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row-start-2">
-          <div className="bg-[#001D3F] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                Oil & Gas EPC - Digitizing P&ID
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+        <Link href="/blogs/why-ai">
+          <div className="row-start-2">
+            <div className="bg-[#001D3F] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  Oil & Gas EPC - Digitizing P&ID
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row-start-2">
-          <div className="w-[402px] h-[328px] bg-no-repeat bg-blog3 relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                How to deal with Unknown Classes during classification?
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+        <Link href="/blogs/why-ai">
+          <div className="row-start-2">
+            <div className="w-[402px] h-[328px] bg-no-repeat bg-blog3 relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  How to deal with Unknown Classes during classification?
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row-start-3">
-          <div className="bg-[#122057] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                The Dilemma: To ML or Not to ML?
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+        <Link href="/blogs/why-ai">
+          <div className="row-start-3">
+            <div className="bg-[#122057] rounded-[18px] w-[403px] h-[327px] relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  The Dilemma: To ML or Not to ML?
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-span-2 ">
-          <div className="w-[833px] h-[327px] bg-no-repeat bg-blog4 relative p-[20px]">
-            <div className="absolute bottom-[20px]">
-              <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
-                How to deal with Unknown Classes during classification?
-              </h3>
-              <div className="flex gap-[10px] items-center">
-                <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
-                <div>
-                  <b className="text-white font-bold font-aeonik tracking-wide ">
-                    Waltor Gottera
-                  </b>
-                  <p className="text-[#FFFFFF] text-lg font-bwmss01">
-                    08 April 2019
-                  </p>
+        </Link>
+        <Link href="/blogs/why-ai">
+          <div className="col-span-2 ">
+            <div className="w-[833px] h-[327px] bg-no-repeat bg-blog4 relative p-[20px]">
+              <div className="absolute bottom-[20px]">
+                <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
+                  How to deal with Unknown Classes during classification?
+                </h3>
+                <div className="flex gap-[10px] items-center">
+                  <div className="border border-[#707070] rounded-full bg-white w-[50px] h-[50px]"></div>
+                  <div>
+                    <b className="text-white font-bold font-aeonik tracking-wide ">
+                      Waltor Gottera
+                    </b>
+                    <p className="text-[#FFFFFF] text-lg font-bwmss01">
+                      08 April 2019
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
