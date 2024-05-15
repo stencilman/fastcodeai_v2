84d1d1077496882925e7b2903fb680c252748a82
abdulfastcode <abdul@fastcode.ai>
2024-05-15 23:14:24 +0530

    contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react'
 import { useGlobalContext } from '../context/GlobalContext'
 import Hero from './sections/Hero';
 import Form from './sections/Form';
+import HowWeDo from '../component/sections/HowWeDo';
+import Clients from '../component/sections/Clients';
+import WhyChooseUs from '../component/sections/WhyChooseUs';
+import Footer from '../component/sections/Footer';
 
 const Contact = () => {
     const { setIsActive } = useGlobalContext();
@@ -14,6 +18,10 @@ const Contact = () => {
         <>
             <Hero/>
             <Form/>
+            <HowWeDo/>
+            <Clients/>
+            <WhyChooseUs/>
+            <Footer/>
         </>
     )
 }
