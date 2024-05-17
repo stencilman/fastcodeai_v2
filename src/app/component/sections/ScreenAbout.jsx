28d96b8857d872a2f64bbeb32baddd521ec7ae2d
abdulfastcode <abdul@fastcode.ai>
2024-05-18 02:56:16 +0530

    client resp

--- src/app/component/sections/ScreenAbout.jsx
+++ src/app/component/sections/ScreenAbout.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const ScreenAbout = () => {
   return (
-    <div className="w-full h-[110vh] md:h-[87vh] lg:h-[100vh]  flex flex-col lg:flex-row relative lg:static gap-[20px] items-center bg-[#00081F]">
+    <div className="w-full h-[90vh] sm:h-[86vh] md:h-[87vh] lg:h-[100vh]  flex flex-col lg:flex-row relative lg:static gap-[20px] items-center bg-[#00081F]">
       <div className="absolute lg:relative left-0 top-[20vw] sm:top-[16vw] lg:top-0">
         <div className="w-[92vw] md:w-[70vw] h-[462px] lg:w-[600px] lg:h-[600px] transform scale-x-[-1]">
           <Lottie animationData={scringAnim} loop={true} />
@@ -17,7 +17,7 @@ const ScreenAbout = () => {
       </div>
       <div className="mb-[100px] absolute lg:static top-[60vw] text-center lg:text-left">
         <h1 className="text-[8vw] lg:text-[51px] text-white font-aeonik tracking-normal relative lg:static top-[-50vw] lg:top-0 ">About FastCodeAI</h1>
-        <p className="text-[#9EB3CF] text-lg w-[100%] lg:w-[80%] px-[10vw] lg:px-0 pt-[28vw] sm:pt-[25vw] md:pt-[8vw] lg:pt-[20px] font-bwmss01">
+        <p className="text-[#9EB3CF] text-base sm:text-lg w-[100%] lg:w-[80%] px-[10vw] lg:px-0 pt-[28vw] sm:pt-[25vw] md:pt-[8vw] lg:pt-[20px] font-bwmss01">
           As a leading AI adoption partner, we empower industry leaders to
           harness cutting-edge intelligence, scale and execute applications, and
           automate operations. We enhance safety, streamline experiences, and
