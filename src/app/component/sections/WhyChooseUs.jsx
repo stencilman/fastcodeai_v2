dfa972d722f795dccc94a3096ed6e8094a8cba16
abdulfastcode <abdul@fastcode.ai>
2024-05-18 17:13:36 +0530

    why choose resp

--- src/app/component/sections/WhyChooseUs.jsx
+++ src/app/component/sections/WhyChooseUs.jsx
@@ -6,13 +6,15 @@ import Image from "next/image";
 
 const WhyChooseUs = () => {
   return (
-    <div className="w-full h-[120vh] pl-[160px] pr-[127px] relative bg-[#00081F] flex flex-col justify-center ">
+    <div className="w-full h-auto lg:h-[120vh] pl-[20px] pr-[20px] lg:pl-[160px] lg:pr-[127px] relative bg-[#00081F] flex flex-col justify-center ">
       <div className="absolute right-[10px]  w-[169px] h-[421px] blur-[50px] translate-y-[167px] scale-90  rounded-[50%] z-[4] bg-gradient-to-br from-[#1D8283] to-[#033577] bg-gradient-156deg bg-no-repeat bg-[0% 0%] opacity-49 border-0 border-opacity-0 filter blur-[100px]"></div>
-      <h1 className="text-5xl  text-white font-aeonik">Why Choose us?</h1>
-      <div className="reltive z-10 grid grid-rows-3 grid-flow-col gap-[27px] w-full h-[auto] mt-[61px]">
-        <div className="row-span-3 bg-gradient-to-br from-[#000E32] to-[#000929] opacity-83 h-[auto] rounded-[18px] w-[413px]">
-          <div className="w-full flex flex-col items-center gap-[125px]">
-            <h3 className="text-[32px] w-[290px] text-white text-center font-aeonik pt-[94px]">
+      <h1 className="text-[8vw] lg:text-5xl  text-white font-aeonik">
+        Why Choose us?
+      </h1>
+      <div className="reltive z-10 grid grid-rows-4 lg:grid-rows-3 grid-flow-col gap-[27px] w-full h-[auto] mt-[61px]">
+        <div className="col-span-3 lg:row-span-3 bg-gradient-to-br from-[#000E32] to-[#000929] opacity-83 h-[auto] rounded-[18px] w-full lg:w-[413px]">
+          <div className="w-full flex flex-col items-center gap-[35px] lg:gap-[125px]">
+            <h3 className="text-[32px] w-[290px] text-white text-center font-aeonik pt-[60px] lg:pt-[94px]">
               <span className="font-bold ">Al</span> Adoption across diverse
               domains
             </h3>
@@ -25,17 +27,24 @@ const WhyChooseUs = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] rounded-[18px]">
-          <div className="flex py-[29px] px-[35px] gap-[30px]">
-            <Image
-              className="relative top-[-20px]"
-              src="/whychooseus/icon1.svg"
-              width="40"
-              height="55"
-              alt="icon"
-            />
+        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
+            <div className="flex w-full gap-[25px] lg:gap-0 items-center">
+              <Image
+                className="w-[45px] md:w-[60px] lg:w-[40px] relative top-0 lg:top-[-20px]"
+                src="/whychooseus/icon1.svg"
+                width="40"
+                height="55"
+                alt="icon"
+              />
+              <h3 className="text-white tracking-normal text-[8vw] font-aeonik lg:hidden">
+                Handpicked Experts
+              </h3>
+            </div>
             <div className="text-white">
-              <h3 className="text-3xl font-aeonik">Handpicked Experts</h3>
+              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                Handpicked Experts
+              </h3>
               <p className="text-lg text-[#9EB3CF] font-bwmss01">
                 Our team is composed of top-tier talent, meticulously selected
                 from the best academic institutions in India. This ensures
@@ -45,17 +54,24 @@ const WhyChooseUs = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] rounded-[18px]">
-          <div className="flex py-[29px] px-[35px] gap-[30px]">
-            <Image
-              className="relative top-[-20px]"
-              src="/whychooseus/icon2.svg"
-              width="40"
-              height="55"
-              alt="icon"
-            />
+        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
+            <div className="flex w-full gap-[25px] lg:gap-0 items-center">
+              <Image
+                className="w-[45px] md:w-[60px] lg:w-[40px] relative top-0 lg:top-[-20px]"
+                src="/whychooseus/icon2.svg"
+                width="40"
+                height="55"
+                alt="icon"
+              />
+              <h3 className="text-white tracking-normal text-[8vw] font-aeonik lg:hidden">
+                Our Process & Approach
+              </h3>
+            </div>
             <div className="text-white">
-              <h3 className="text-3xl font-aeonik">Our Process & Approach</h3>
+              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                Our Process & Approach
+              </h3>
               <p className="text-lg text-[#9EB3CF] font-bwmss01">
                 We offer flexible engagement models tailored to each{" "}
                 {"client's"} specific needs.W hether {"it's"} for Web, Mobile,
@@ -65,17 +81,24 @@ const WhyChooseUs = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] rounded-[18px]">
-          <div className="flex py-[29px] px-[35px] gap-[30px]">
-            <Image
-              className="relative top-[-20px]"
-              src="/whychooseus/icon3.svg"
-              width="40"
-              height="55"
-              alt="icon"
-            />
+        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
+            <div className="flex w-full gap-[25px] lg:gap-0 items-center">
+              <Image
+                className="w-[45px] md:w-[60px] lg:w-[40px] relative top-0 lg:top-[-20px]"
+                src="/whychooseus/icon3.svg"
+                width="40"
+                height="55"
+                alt="icon"
+              />
+              <h3 className="text-white tracking-normal text-[8vw] font-aeonik lg:hidden">
+                Strong R & D Expertise
+              </h3>
+            </div>
             <div className="text-white">
-              <h3 className="text-3xl font-aeonik">Strong R & D Expertise</h3>
+              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                Strong R & D Expertise
+              </h3>
               <p className="text-lg text-[#9EB3CF] font-bwmss01">
                 Over ten years of pioneering AI innovations and multiple patents
                 highlight our deep research and development capabilities. This
