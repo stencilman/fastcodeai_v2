53b325705a4d83df458820306a33d46ce4fa6464
abdulfastcode <abdul@fastcode.ai>
2024-05-18 05:30:50 +0530

    team respo

--- src/app/component/sections/Team.jsx
+++ src/app/component/sections/Team.jsx
@@ -18,7 +18,7 @@ const Team = () => {
       "/team/Nanda.webp",
       "/team/parth-bw.webp",
       "/team/Prabal.webp",
-      '/team/Sanjay.webp',
+      "/team/Sanjay.webp",
       // '/team/Shreyas.webp',
       // '/team/Sughosh.webp',
       // '/team/Tirth.webp'
@@ -57,14 +57,18 @@ const Team = () => {
 
   return (
     <div className="w-full h-[100vh] bg-gradient-to-br from-[#000E32] to-[#000929] opacity-83 relative flex flex-col items-center justify-center z-[4]">
-      <div className="flex gap-[0px] w-full items-center z-[3]">
-        <div className="pl-[100px] w-[45%]">
-          <h1 className="text-[51px] text-white font-aeonik">Our Team</h1>
-          <p className="text-[#9EB3CF] w-[80%] text-[19px] font-bwmss01 font-light mt-[30px]">
-          Our team consists of seasoned professionals with extensive research backgrounds, authors of influential papers, and contributors to open-source initiatives. Carefully selected from top universities, with a premium placed on innovative problem solvers.
-
+      <div className="flex flex-col md:flex-row gap-[10vw] md:gap-[0px] w-full items-center z-[3]">
+        <div className="pl-[20px] md:pl-[50px] lg:pl-[100px] w-[100%] md:w-[45%]">
+          <h1 className="text-[8vw] lg:text-[51px] text-white font-aeonik">
+            Our Team
+          </h1>
+          <p className="text-[#9EB3CF] w-[80%] text-sm md:text-lg font-bwmss01 font-light mt-[15px] md:mt-[30px]">
+            Our team consists of seasoned professionals with extensive research
+            backgrounds, authors of influential papers, and contributors to
+            open-source initiatives. Carefully selected from top universities,
+            with a premium placed on innovative problem solvers.
           </p>
-          <button className="rounded-[28px] mt-[50px] flex items-center gap-[7px] bg-gradient-to-br from-[#2DC1C3] to-[#0268F2] text-white p-[15px] text-lg">
+          <button className="rounded-[28px] mt-[20px] md:mt-[35px] lg:mt-[50px] flex items-center gap-[7px] bg-gradient-to-br from-[#2DC1C3] to-[#0268F2] text-white p-[15px] text-lg">
             Get Started
             <Image
               className="inline"
@@ -75,10 +79,15 @@ const Team = () => {
             />
           </button>
         </div>
-        <div className="columns__col mod--team is-2 w-[55%] h-full pr-[127px]">
-          <div className="team_grid flex flex-wrap justify-end">
+        <div className="columns__col mod--team is-2 w-[100%] md:w-[55%] h-full md:pr-[50px] lg:pr-[127px]">
+          <div className="team_grid flex flex-wrap justify-center md:justify-end">
             {imageInfo.map((info, index) => (
-              <div key={info.src} className="team__grid-block p-4">
+              <div
+                key={info.src}
+                className={`team__grid-block p-4 ${
+                  index > 7 ? "hidden lg:block" : "block"
+                }`}
+              >
                 <div
                   className={`team_circle rounded-full w-[7.5rem] h-[7.5rem] transition-all duration-300 relative overflow-hidden ease-cubic-bezier ${
                     info.visible ? "border-0" : "border border-[#ffffffcc]"
