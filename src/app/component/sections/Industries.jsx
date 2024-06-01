0c76ff02239d819fecf72642afe9fb4c20475881
abdulfastcode <abdul@fastcode.ai>
2024-06-01 18:44:25 +0530

    indust txt sng line

--- src/app/component/sections/Industries.jsx
+++ src/app/component/sections/Industries.jsx
@@ -5,7 +5,7 @@ const Industries = () => {
   return (
     <div className="w-full h-auto mt-[100px] md:mt-[100px] lg:mt-[0px] lg:min-h-[80vh] pt-[100px] lg:pt-0 pl-[20px] pr-[20px] md:pl-[50px] md:pr-[20px] lg:pl-[160px] lg:pr-[127px] bg-[#00081F] flex flex-col justify-center relative ">
       <div className="flex flex-col items-center">
-        <h3 className="text-[8vw] md:text-[7vw] lg:text-5xl lg:leading-[3.7rem] tracking-normal  text-center leading-[7.6vw] w-[90vw] lg:w-[50vw] font-bwmss01 text-white">
+        <h3 className="text-[8vw] md:text-[7vw] lg:text-5xl lg:leading-[3.7rem] tracking-normal  text-center leading-[7.6vw]  font-bwmss01 text-white">
         Our AI Expertise Across Industries
         </h3>
         {/* <p className="text-[#9EB3CF] text-lg mt-[20px] text-center w-[85vw] lg:w-[70vw] font-bwmss01">
