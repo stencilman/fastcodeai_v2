7800ea520bc268100abde26a4aa718c2ae6e073c
abdulfastcode <abdul@fastcode.ai>
2024-06-11 17:01:39 +0530

    fix scrl

--- src/app/component/sections/Footer.jsx
+++ src/app/component/sections/Footer.jsx
@@ -156,15 +156,15 @@ const Footer = ({ showExtraSpace }) => {
             <motion.h1
               initial={{ x: "0" }}
               animate={{ x: "-100%" }}
-              transition={{ repeat: Infinity, ease: "linear", duration: 8 }}
-              className="text-[9.5vw] leading-none font-medium  tracking-tight text-[#37446A]  w-full mr-[8px] "
+              transition={{ repeat: Infinity, ease: "linear", duration: 7 }}
+              className="text-[9.5vw] leading-none font-medium  tracking-tight text-[#37446A]  w-full mr-[6px] "
             >
               {"Get Free Consult Now!"}
             </motion.h1>
             <motion.h1
               initial={{ x: "0" }}
               animate={{ x: "-100%" }}
-              transition={{ repeat: Infinity, ease: "linear", duration: 8 }}
+              transition={{ repeat: Infinity, ease: "linear", duration: 7 }}
               className="text-[9.5vw] leading-none font-medium tracking-tight text-[#37446A]  w-full "
             >
               {"Get Free Consult Now!"}
