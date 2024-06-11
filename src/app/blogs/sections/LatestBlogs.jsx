0043d174202c9eab9c206028402441be3c830dda
abdulfastcode <abdul@fastcode.ai>
2024-06-11 23:33:24 +0530

    blog fix

--- src/app/blogs/sections/LatestBlogs.jsx
+++ src/app/blogs/sections/LatestBlogs.jsx
@@ -11,10 +11,10 @@ const LatestBlogs = () => {
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 grid-rows-7 md:grid-cols-2 md:grid-rows-3 lg:grid-rows-4 2xl:grid-cols-3 2xl:grid-rows-3 gap-[30px] w-full">
+      <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-2 lg:grid-rows-2 2xl:grid-cols-3 2xl:grid-rows-2 gap-[30px] w-full">
         <div className=" lg:col-span-2">
           <Link href="/blogs/why-ai">
-            <div className="rounded-[20px] w-full bg-cover 2xl:w-[833px] h-[327px] bg-no-repeat bg-blog1 relative p-[20px]">
+            <div className="rounded-[60px] w-full bg-cover 2xl:w-[833px] h-[327px] bg-no-repeat bg-blog1 relative p-[20px]">
               <div className="absolute bottom-[20px]">
                 <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
                   The Dilemma: To ML or Not to ML?
@@ -43,6 +43,7 @@ const LatestBlogs = () => {
           </Link>
         </div>
 
+{/* first commented */}
         {/* <Link href="/blogs/why-ai">
           <div className=" lg:col-start-3 ">
             <div className="bg-[#104E62] rounded-[20px] lg:w-[403px] h-[327px] relative p-[20px]">
@@ -65,7 +66,9 @@ const LatestBlogs = () => {
             </div>
           </div>
         </Link> */}
-        <Link href="/blogs/unknown-classes">
+
+
+        {/* <Link href="/blogs/unknown-classes">
           <div className="row-start-2">
             <div className="w-full bg-cover rounded-[20px] 2xl:w-[402px] h-[328px] bg-no-repeat bg-blog2 relative p-[20px]">
               <div className="absolute bottom-[20px]">
@@ -94,8 +97,8 @@ const LatestBlogs = () => {
               </div>
             </div>
           </div>
-        </Link>
-        <Link href="/blogs/oil-gas">
+        </Link> */}
+        {/* <Link href="/blogs/oil-gas">
           <div className="row-start-2">
             <div className="bg-[#001D3F] w-full rounded-[20px] 2xl:w-[403px] h-[327px] relative p-[20px]">
               <div className="absolute bottom-[20px]">
@@ -116,8 +119,8 @@ const LatestBlogs = () => {
               </div>
             </div>
           </div>
-        </Link>
-        <Link href="/blogs/why-ai">
+        </Link> */}
+        {/* <Link href="/blogs/why-ai">
           <div className="row-start-3">
             <div className="bg-[#122057] rounded-[20px] w-full 2xl:w-[403px] h-[327px] relative p-[20px]">
               <div className="absolute bottom-[20px]">
@@ -146,10 +149,11 @@ const LatestBlogs = () => {
               </div>
             </div>
           </div>
-        </Link>
+        </Link> */}
+        <div className="lg:col-span-2 2xl:col-span-3">
         <Link href="/blogs/oil-gas">
-          <div className="row-start-2">
-            <div className="bg-cover rounded-[20px]  w-full 2xl:w-[402px] h-[328px] bg-no-repeat bg-blog3 relative p-[20px]">
+          <div className="">
+            <div className="bg-cover rounded-[60px]  w-full 2xl:w-[402px] h-[328px] bg-no-repeat bg-blog3 relative p-[20px]">
               <div className="absolute bottom-[20px]">
                 <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
                   Oil & Gas EPC - Digitizing P&ID
@@ -177,13 +181,14 @@ const LatestBlogs = () => {
             </div>
           </div>
         </Link>
+        </div>
 
         <Link
           href="/blogs/unknown-classes"
           className="lg:col-span-2 2xl:col-span-3"
         >
           <div>
-            <div className="rounded-[20px] w-full bg-cover 2xl:w-[833px] h-[327px] bg-no-repeat bg-blog4 relative p-[20px]">
+            <div className="rounded-[60px] w-full bg-cover 2xl:w-[833px] h-[327px] bg-no-repeat bg-blog4 relative p-[20px]">
               <div className="absolute bottom-[20px]">
                 <h3 className="text-white text-2xl font-aeonik tracking-wide mb-[15px]">
                   How to deal with Unknown Classes during classification?
