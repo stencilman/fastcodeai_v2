6eb3fa35f9df23304a0bef8c52e473c80d265cdd
abdulfastcode <abdul@fastcode.ai>
2024-06-12 19:37:29 +0530

    fixed

--- src/app/component/Preloader.jsx
+++ src/app/component/Preloader.jsx
@@ -11,7 +11,7 @@ const Preloader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[#00081F] text-white z-50 transition-opacity duration-1000">
       {/* <h1 className="text-4xl font-bold animate-pulse">Welcome to My Website</h1> */}
-      <div className="w-[80%] animate-pulse">
+      <div className="w-[80%] ">
           <Lottie animationData={preloader1} loop={false} />
         </div>
     </div>
