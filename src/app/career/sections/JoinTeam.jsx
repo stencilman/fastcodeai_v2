1852695f33893c27edf64f4ee578023e6e731233
abdulfastcode <abdul@fastcode.ai>
2024-06-25 19:52:41 +0530

    border anim

--- src/app/career/sections/JoinTeam.jsx
+++ src/app/career/sections/JoinTeam.jsx
@@ -1,4 +1,5 @@
 import Button from "@/app/component/Button";
+import RoundedHover from "@/app/component/RoundedHover";
 import Image from "next/image";
 import React from "react";
 
@@ -32,112 +33,125 @@ const JoinTeam = () => {
           </p>
 
           <div className="max-w-[200px]">
+          
             <div
+              className="rounded-[28px] relative z-[1] bg-gradient-to-br from-[#2DC1C3] to-[#0268F2] group  text-lg justify-between cursor-pointer max-w-[200px] fill-[#fff] hover:fill-[#00092A] text-white hover:text-[#00092A] duration-500 transition-all ease-linear"
+              style={{ overflow: "hidden" }}
               onClick={() => scrollToPositions("positions")}
-              className="rounded-[28px] relative z-[1] flex items-center gap-[7px] bg-gradient-to-br from-[#2DC1C3] to-[#0268F2] text-white p-[15px] text-lg justify-between cursor-pointer"
             >
-              <div>Explore Positions</div>
-              <div>
-                <Image
-                  className="inline "
-                  src="/arrowRight.svg"
-                  alt="arrow"
-                  width="16"
-                  height="13"
-                />
-              </div>
+              <RoundedHover>
+                <div className="p-[15px] relative z-10 flex items-center gap-[7px] ">
+                  Explore Positions
+                  <svg
+                    className="w-[16px] h-[13px] duration-500 transition-all ease-linear group-hover:fill-[#00092A] fill-[#fff] relative z-10 "
+                    viewBox="0 0 46 40"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"></path>
+                  </svg>
+                </div>
+              </RoundedHover>
+             
             </div>
           </div>
         </div>
         <div className="w-full lg:w-[77%] xl:[60%]">
           <div className="w-full flex flex-wrap justify-center gap-[20px] md:gap-[30px]">
-            <div
-              className="px-[20px] min-w-full sm:min-w-[302px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center max-w-[278px] gap-[14px]"
-              style={{
-                background:
-                  "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
-              }}
-            >
+          <div className="moving-border-wrapper rounded-[18px] h-[118px] min-w-full sm:min-w-[302px] max-w-[278px] ">
               <div
-                className="rounded-[14px] border-[#1A2758] p-[6px]"
+                className="moving-border-content  px-[20px]  py-[20px]  md:py-[28px] border border-[#1A2758]  flex justify-center items-center  gap-[14px]"
                 style={{
                   background:
-                    "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                    "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
                 }}
               >
-                <b className="text-white font-aeonik text-[2rem] md:text-5xl">
-                  15+
-                </b>
+                <div
+                  className="rounded-[14px] border-[#1A2758] p-[6px]"
+                  style={{
+                    background:
+                      "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                  }}
+                >
+                  <b className="text-white font-aeonik text-[2rem] md:text-5xl">
+                    15+
+                  </b>
+                </div>
+                <p className="text-center text-white text-2xl font-bwmss01 font-bold">
+                  Experience
+                </p>
               </div>
-              <p className="text-center text-white text-2xl font-bwmss01 font-bold">
-                Experience
-              </p>
             </div>
-            <div
-              className="px-[20px] min-w-full sm:min-w-[302px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center max-w-[278px] gap-[14px]"
-              style={{
-                background:
-                  "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
-              }}
-            >
+            <div className="moving-border-wrapper rounded-[18px]  h-[118px] min-w-full sm:min-w-[302px] max-w-[278px]">
               <div
-                className="rounded-[14px] border-[#1A2758] p-[6px]"
+                className="moving-border-content px-[20px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center  gap-[14px]"
                 style={{
                   background:
-                    "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                    "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
                 }}
               >
-                <b className="text-white font-aeonik text-[2rem] md:text-5xl">
-                  30+
-                </b>
+                <div
+                  className="rounded-[14px] border-[#1A2758] p-[6px]"
+                  style={{
+                    background:
+                      "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                  }}
+                >
+                  <b className="text-white font-aeonik text-[2rem] md:text-5xl">
+                    30+
+                  </b>
+                </div>
+                <p className="text-center text-white text-2xl font-bwmss01 font-bold">
+                  Projects
+                </p>
               </div>
-              <p className="text-center text-white text-2xl font-bwmss01 font-bold">
-                Projects
-              </p>
             </div>
-            <div
-              className="px-[20px] min-w-full sm:min-w-[302px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center max-w-[278px] gap-[14px]"
-              style={{
-                background:
-                  "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
-              }}
-            >
+            <div className="moving-border-wrapper rounded-[18px]  h-[118px] min-w-full sm:min-w-[302px] max-w-[278px]">
               <div
-                className="rounded-[14px] border-[#1A2758] p-[6px]"
+                className="moving-border-content px-[20px]  py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center  gap-[14px]"
                 style={{
                   background:
-                    "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                    "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
                 }}
               >
-                <b className="text-white font-aeonik text-[2rem] md:text-5xl">
-                  7000+
-                </b>
+                <div
+                  className="rounded-[14px] border-[#1A2758] p-[6px]"
+                  style={{
+                    background:
+                      "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                  }}
+                >
+                  <b className="text-white font-aeonik text-[2rem] md:text-5xl">
+                    7000+
+                  </b>
+                </div>
+                <p className="text-center text-white text-2xl font-bwmss01 font-bold">
+                  Citations
+                </p>
               </div>
-              <p className="text-center text-white text-2xl font-bwmss01 font-bold">
-                Citations
-              </p>
             </div>
-            <div
-              className="px-[20px] min-w-full sm:min-w-[302px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center max-w-[278px] gap-[14px]"
-              style={{
-                background:
-                  "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
-              }}
-            >
+            <div className="moving-border-wrapper rounded-[18px]  h-[118px] min-w-full sm:min-w-[302px] max-w-[278px]">
               <div
-                className="rounded-[14px] border-[#1A2758] p-[6px]"
+                className="moving-border-content px-[20px] py-[20px]  md:py-[28px] border border-[#1A2758] rounded-[18px] flex justify-center items-center  gap-[14px]"
                 style={{
                   background:
-                    "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                    "transparent linear-gradient(360deg, #13224F 0%, #13224F 0%, #0C1C46 51%, #020F3B 100%) 0% 0% no-repeat padding-box",
                 }}
               >
-                <b className="text-white font-aeonik text-[2rem] md:text-5xl">
-                  20+
-                </b>
+                <div
+                  className="rounded-[14px] border-[#1A2758] p-[6px]"
+                  style={{
+                    background:
+                      "transparent linear-gradient(360deg, #13224F 0%, #010D33 0%, #010D34 51%, #020B2A 100%) 0% 0% no-repeat padding-box",
+                  }}
+                >
+                  <b className="text-white font-aeonik text-[2rem] md:text-5xl">
+                    20+
+                  </b>
+                </div>
+                <p className="text-center text-white text-2xl font-bwmss01 font-bold">
+                  Clients
+                </p>
               </div>
-              <p className="text-center text-white text-2xl font-bwmss01 font-bold">
-                Clients
-              </p>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes:{
+        "border-spin":{
+          "100%":{
+            transform: "rotate(-360deg)"
+          }
+        }
+      },
+      animation :{
+        "border-spin":'border-spin 7s linear infinite'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
