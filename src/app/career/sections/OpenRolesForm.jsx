d8f701a3cdcab8c963d26af07cfd2eb5c138e8a2
abdulfastcode <abdul@fastcode.ai>
2024-07-02 15:14:16 +0530

    career : flutr dev added

--- src/app/career/sections/OpenRolesForm.jsx
+++ src/app/career/sections/OpenRolesForm.jsx
@@ -31,11 +31,10 @@ const OpenRolesForm = () => {
             <div className=" flex  flex-col mt-[30px] px-0 md:px-[32px] ">
               <div
                 onClick={() => {
-                  
                   handleAccordion(0);
                   setIsOpen(!isOpen);
                 }}
-                className="flex justify-between items-center"
+                className="flex justify-between items-center select-none"
               >
                 <h1
                   className={`text-[7vw] md:text-[6vw] lg:text-4xl text-white font-aeonik transition-all duration-300 ease-in-out tracking-normal ${
@@ -106,7 +105,86 @@ const OpenRolesForm = () => {
                   <div className=" mt-[25px]">
                     <ul className="flex pl-[0px] flex-wrap gap-[15px] ">
                       <li className="text-white font-aeonik text-xl font-medium">
-                        Bangalore, India
+                        Bengaluru, India
+                      </li>
+                      <li className="text-white font-aeonik text-xl font-medium">
+                        Fulltime
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          </div>
+
+          {/* Accordion 2 */}
+          <div className="group border-b-2 pb-[30px] border-[#AFC0FF] cursor-pointer w-[100%] flex-col ">
+            <div className=" flex  flex-col mt-[30px] px-0 md:px-[32px] ">
+              <div
+                onClick={() => {
+                  handleAccordion(1);
+                  setIsOpen(!isOpen);
+                }}
+                className="flex justify-between items-center select-none"
+              >
+                <h1
+                  className={`text-[7vw] md:text-[6vw] lg:text-4xl text-white font-aeonik transition-all duration-300 ease-in-out tracking-normal ${
+                    showAccordion !== 1 ? "group-hover:scale-[1.1]" : ""
+                  }`}
+                >
+                  Flutter Developer
+                </h1>
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  className=" py-3 px-4 focus:outline-none"
+                >
+                  <motion.div
+                    animate={{
+                      rotate: showAccordion === 1 && isOpen ? 180 : 0,
+                    }}
+                    transition={{ duration: 0.3 }}
+                    className="transform"
+                  >
+                    <svg
+                      className="w-5 h-5 md:w-6 md:h-6 text-white"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={4}
+                        d="M19 9l-7 7-7-7"
+                      />
+                    </svg>
+                  </motion.div>
+                </button>
+              </div>
+              <motion.div
+                initial={false}
+                animate={showAccordion === 1 && isOpen ? "open" : "closed"}
+                variants={{
+                  open: { opacity: 1, height: "auto" },
+                  closed: { opacity: 0, height: 0, duration: 0.6 },
+                }}
+                transition={{ duration: 0.6 }}
+                className="overflow-hidden"
+              >
+                <div className="cursor-default">
+                  <div className="flex w-[100%] justify-between md:items-center flex-col md:flex-row gap-[30px] md:gap-0">
+                    <p className="text-[#9EB3CF] font-bwmss01 w-[100%] md:w-[61%] text-base md:text-lg pt-[25px]">
+                    We are excited to offer a full-time role for a Flutter Developer with 2+ years of proven experience in Flutter development based in our Bengaluru office. As part of our dynamic team, you will be tasked with developing and maintaining state-of-the-art mobile applications using the Flutter framework. Your role will involve designing user interfaces, implementing functionality, testing, debugging, and collaborating with cross-functional teams to enhance application performance and user experience.
+                    </p>
+                    <div className="max-w-[160px] mr-[60px]">
+                      <Button name="Apply Now" to="/contact" />
+                    </div>
+                  </div>
+                  <div className=" mt-[25px]">
+                    <ul className="flex pl-[0px] flex-wrap gap-[15px] ">
+                      <li className="text-white font-aeonik text-xl font-medium">
+                        Bengaluru, India
                       </li>
                       <li className="text-white font-aeonik text-xl font-medium">
                         Fulltime
--- src/app/component/sections/WhyChooseUs.jsx
+++ src/app/component/sections/WhyChooseUs.jsx
@@ -1,160 +1,150 @@
 "use client";
 import Lottie from "lottie-react";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import chooseUS from "../../../../public/animations/chooseUS.json";
 import Image from "next/image";
 import icon1 from "../../../../public/whychooseus/icon1.png";
 import icon2 from "../../../../public/whychooseus/icon2.png";
 import icon3 from "../../../../public/whychooseus/icon3.png";
+import { motion, useInView, useAnimation } from "framer-motion";
+import RevealX from "../RevealX";
+import RevealY from "../RevealY";
 
 const WhyChooseUs = () => {
+  // const ref = useRef(null);
+  // const isInView = useInView(ref, { once: true });
+
+  // const mainControls = useAnimation();
+
+  // useEffect(() => {
+  //   console.log("isInView", isInView);
+  //   if (isInView) {
+  //     mainControls.start("visible");
+  //   }
+  // }, [isInView]);
+
   return (
     <div className="w-full h-auto lg:min-h-[120vh] my-[75px] pl-[20px] pr-[20px] lg:pl-[120px] lg:pr-[100px] relative bg-[#00081F] flex flex-col justify-center ">
       <div className="absolute right-[10px]  w-[169px] h-[421px] blur-[50px] translate-y-[167px] scale-90  rounded-[50%] z-[4] bg-gradient-to-br from-[#1D8283] to-[#033577] bg-gradient-156deg bg-no-repeat bg-[0% 0%] opacity-49 border-0 border-opacity-0 filter blur-[100px]"></div>
       <h1 className="text-[8vw] md:text-[7vw] lg:text-5xl  text-white font-aeonik">
-        Why Choose us?
+        Why Choose Us?
       </h1>
-      <div className="reltive z-10 grid grid-col-[1fr] lg:grid-cols-[0fr] grid-rows-4 lg:grid-rows-3 grid-flow-col gap-[27px] w-full h-[auto] mt-[61px]">
-        <div className="col-span-3 lg:row-span-3 bg-gradient-to-br from-[#000E32] to-[#000929] opacity-83 h-[auto] rounded-[18px] w-full lg:w-[413px]">
-          <div className="w-full flex flex-col items-center gap-[35px] lg:gap-[125px]">
-            <h3 className="text-[32px] w-[290px] text-white text-center font-aeonik pt-[60px] lg:pt-[94px]">
-              <span className="font-bold ">Al</span> Adoption across diverse
-              domains
-            </h3>
-            <div className="w-[300px] sm:w-[365px] md:w-[413px] h-[332px] relative left-[-29px] lg:left-[-38px]">
-              <Lottie
-                style={{ width: "117%" }}
-                animationData={chooseUS}
-                loop={true}
-              />
-            </div>
-          </div>
+      <div className="relative z-10 flex flex-col lg:flex-row   gap-[27px] w-full h-[auto] mt-[61px]">
+        {/* <div ref={ref} className="relative flex-1  overflow-hidden w-full h-[auto]">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, x: -75 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ duration: 0.5, delay: 0.25 }}
+          > */}
+         
+            <div className="flex-1 bg-gradient-to-br from-[#000E32] to-[#000929] opacity-83 h-[auto] rounded-[18px] w-full ">
+              <div className="w-full flex flex-col items-center gap-[35px] lg:gap-[125px]">
+                <h3 className="text-[32px]  w-[290px] text-white text-center font-aeonik pt-[60px] lg:pt-[94px]">
+                  <span className="font-bold ">Al</span> Adoption across diverse
+                  domains
+                </h3>
+                <div className="w-[300px] sm:w-[365px] md:w-[413px] h-[250px] md:h-[332px] relative left-[-29px] lg:left-[-38px]">
+                  <Lottie
+                    style={{ width: "117%" }}
+                    animationData={chooseUS}
+                    loop={true}
+                  />
+                </div>
+              </div>
+            {/* </div>
+          </motion.div> */}
         </div>
-        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
-          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
-            <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
-              <Image
-                className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
-                src={icon1}
-           
-                width="40"
-                height="55"
-                alt="icon"
-              />
-              <h3 className="text-white tracking-normal text-[8vw] md:text-[7vw] font-aeonik lg:hidden leading-[8vw]">
-                Handpicked Experts
-              </h3>
-            </div>
-            <div className="text-white">
-              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
-                Handpicked Experts
-              </h3>
-              <p className="text-lg text-[#9EB3CF] font-bwmss01">
-              Comprised of elite professionals sourced from {"India's"} finest academic institutions, our team offers unparalleled expertise to catalyze your innovation.
-              </p>
+        <div className="flex gap-[27px] flex-col">
+          <RevealY>
+          <div className=" bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+            <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
+              <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
+                <Image
+                  className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
+                  src={icon1}
+                  width="40"
+                  height="55"
+                  alt="icon"
+                />
+                <h3 className="text-white tracking-normal text-[30px] sm:text-[35px] leading-[40px] font-aeonik lg:hidden ">
+                  Handpicked Experts
+                </h3>
+              </div>
+              <div className="text-white">
+                <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                  Handpicked Experts
+                </h3>
+                <p className="text-lg text-[#9EB3CF] font-bwmss01">
+                  Comprised of elite professionals sourced from {"India's"}{" "}
+                  finest academic institutions, our team offers unparalleled
+                  expertise to catalyze your innovation.
+                </p>
+              </div>
             </div>
           </div>
+          </RevealY>
+          <RevealY>
+          <div className=" bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+            <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
+              <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
+                <Image
+                  className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
+                  src={icon2}
+                  width="40"
+                  height="55"
+                  alt="icon"
+                />
+                <h3 className="text-white tracking-normal text-[30px] sm:text-[35px] leading-[40px] font-aeonik lg:hidden ">
+                  Our Process & Approach
+                </h3>
+              </div>
+              <div className="text-white">
+                <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                  Our Process & Approach
+                </h3>
+                <p className="text-lg text-[#9EB3CF] font-bwmss01">
+                  Tailored engagement models for Web, Mobile, IoT, and Edge
+                  computing ensure fluid project progression from initial
+                  concept to full deployment.
+                </p>
+              </div>
+            </div>
+          </div>
+          </RevealY>
+          <RevealY>
+          <div className=" bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
+            <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[4vw] items-center justify-center lg:justify-normal w-full h-full">
+              <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
+                <Image
+                  className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
+                  src={icon3}
+                  width="40"
+                  height="55"
+                  alt="icon"
+                />
+                <h3 className="text-white tracking-normal text-[30px] sm:text-[35px] leading-[40px] font-aeonik lg:hidden ">
+                  Strong R & D Expertise
+                </h3>
+              </div>
+              <div className="text-white">
+                <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
+                  Strong R & D Expertise
+                </h3>
+                <p className="text-lg text-[#9EB3CF] font-bwmss01">
+                  A decade of leading AI innovation, underscored by numerous
+                  patents and publications, showcases our robust research and
+                  development prowess, equipped to resolve complex challenges
+                  and excel in delivery.
+                </p>
+              </div>
+            </div>
+          </div>
+          </RevealY>
         </div>
-        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
-          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px] items-center justify-center lg:justify-normal w-full h-full">
-            <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
-              <Image
-                className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
-                src={icon2}
-           
-                width="40"
-                height="55"
-                alt="icon"
-              />
-              <h3 className="text-white tracking-normal text-[8vw] md:text-[7vw] font-aeonik lg:hidden leading-[8vw]">
-              Our Process & Approach 
-              </h3>
-            </div>
-            <div className="text-white">
-              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
-              Our Process & Approach 
-              </h3>
-              <p className="text-lg text-[#9EB3CF] font-bwmss01">
-              Tailored engagement models for Web, Mobile, IoT, and Edge computing ensure fluid project progression from initial concept to full deployment.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
-          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[4vw] items-center justify-center lg:justify-normal w-full h-full">
-            <div className="flex w-full lg:w-[200px] gap-[25px] lg:gap-0 items-center lg:justify-center">
-              <Image
-                className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
-                src={icon3}
-           
-                width="40"
-                height="55"
-                alt="icon"
-              />
-              <h3 className="text-white tracking-normal text-[8vw] md:text-[7vw] font-aeonik lg:hidden leading-[8vw]">
-              Strong R & D Expertise
-              </h3>
-            </div>
-            <div className="text-white">
-              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
-              Strong R & D Expertise
-              </h3>
-              <p className="text-lg text-[#9EB3CF] font-bwmss01">
-              A decade of leading AI innovation, underscored by numerous patents and publications, showcases our robust research and development prowess, equipped to resolve complex challenges and excel in delivery.
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
-          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[30px]  items-center justify-center lg:justify-normal w-full h-full">
-            <div className="flex w-full lg:w-[50%] gap-[25px] lg:gap-0 items-center lg:justify-center">
-              <Image
-                className="w-[45px] md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
-                src={icon2}
-          
-                width="40"
-                height="55"
-                alt="icon"
-              />
-              <h3 className="text-white tracking-normal text-[8vw] md:text-[7vw] font-aeonik lg:hidden leading-[8vw]">
-                Our Process & Approach
-              </h3>
-            </div>
-            <div className="text-white">
-              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
-                Our Process & Approach
-              </h3>
-              <p className="text-lg text-[#9EB3CF] font-bwmss01">
-              Tailored engagement models for Web, Mobile, IoT, and Edge computing ensure fluid project progression from initial concept to full deployment.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-3 lg:col-span-2 bg-gradient-to-br from-[#000E32] to-[#000929] h-[auto] lg:h-[auto] rounded-[18px]">
-          <div className="flex flex-col lg:flex-row py-[29px] px-[35px] gap-[20px]  items-center justify-center lg:justify-normal w-full h-full">
-            <div className="flex lg:w-[50%] w-full gap-[25px] lg:gap-0 items-center lg:justify-center">
-              <Image
-                className="w-[45px]  md:w-[60px] lg:w-[70px] lg:mt-[20px] relative top-0 lg:top-[-20px]"
-                src={icon3}
-               
-                width="40"
-                height="55"
-                alt="icon"
-              />
-              <h3 className="text-white tracking-normal text-[8vw] md:text-[7vw] font-aeonik lg:hidden leading-[8vw]">
-                Strong R & D Expertise
-              </h3>
-            </div>
-            <div className="text-white">
-              <h3 className="hidden tracking-normal lg:block text-3xl font-aeonik">
-                Strong R & D Expertise
-              </h3>
-              <p className="text-lg text-[#9EB3CF] font-bwmss01">
-              A decade of leading AI innovation, underscored by numerous patents, showcases our robust research and development prowess, equipped to resolve complex challenges and excel in delivery.
-              </p>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
